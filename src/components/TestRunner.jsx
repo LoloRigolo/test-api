fec219086caa4c51af3156d61333576aaf1b7a06
nolan25 <favandnolan@gmail.com>
2025-04-14 14:36:09 +0200

    test

--- src/components/TestRunner.jsx
+++ src/components/TestRunner.jsx
@@ -80,7 +80,7 @@ export default function TestRunner() {
             <button
               onClick={() => handleRemoveCampaign(c.id)}
               className="remove-btn"
-              title="Supprimer ce blocs"
+              title="Supprimer ce bloc"
             >
               ❌
             </button>
