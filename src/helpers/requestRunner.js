bdbfab9d7989455c7aff4cdfbfc5f13682f40f13
nolan25 <favandnolan@gmail.com>
2025-04-14 11:32:02 +0200

    get/post

--- src/helpers/requestRunner.js
+++ src/helpers/requestRunner.js
@@ -1,12 +1,5 @@
-export async function runTest({ url, totalRequests, delay, onResult }) {
-  const stats = {
-    total: 0,
-    success: 0,
-    networkErrors: 0,
-    http4xx: 0,
-    http5xx: 0,
-    durations: [],
-  };
+export async function runGetTest({ url, totalRequests, delay, onResult }) {
+  const stats = createEmptyStats();
 
   for (let i = 0; i < totalRequests; i++) {
     try {
@@ -14,28 +7,79 @@ export async function runTest({ url, totalRequests, delay, onResult }) {
       const res = await fetch(url);
       const time = performance.now() - start;
 
-      stats.total++;
-      stats.durations.push(time);
-
-      if (res.ok) {
-        stats.success++;
-      } else if (res.status >= 400 && res.status < 500) {
-        stats.http4xx++;
-      } else if (res.status >= 500) {
-        stats.http5xx++;
-      }
+      updateStats(stats, res, time);
     } catch (e) {
       stats.total++;
       stats.networkErrors++;
-      console.log(e);
     }
 
     onResult?.(stats, i + 1);
 
-    if (delay > 0) {
-      await new Promise((res) => setTimeout(res, delay));
-    }
+    if (delay > 0) await wait(delay);
   }
 
   return stats;
+}
+
+export async function runPostTest({
+  url,
+  totalRequests,
+  delay,
+  body,
+  onResult,
+}) {
+  const stats = createEmptyStats();
+
+  for (let i = 0; i < totalRequests; i++) {
+    try {
+      const start = performance.now();
+      const res = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+      const time = performance.now() - start;
+
+      updateStats(stats, res, time);
+    } catch (e) {
+      stats.total++;
+      stats.networkErrors++;
+    }
+
+    onResult?.(stats, i + 1);
+
+    if (delay > 0) await wait(delay);
+  }
+
+  return stats;
+}
+
+function createEmptyStats() {
+  return {
+    total: 0,
+    success: 0,
+    networkErrors: 0,
+    http4xx: 0,
+    http5xx: 0,
+    durations: [],
+  };
+}
+
+function updateStats(stats, res, duration) {
+  stats.total++;
+  stats.durations.push(duration);
+
+  if (res.ok) {
+    stats.success++;
+  } else if (res.status >= 400 && res.status < 500) {
+    stats.http4xx++;
+  } else if (res.status >= 500) {
+    stats.http5xx++;
+  }
+}
+
+function wait(ms) {
+  return new Promise((res) => setTimeout(res, ms));
 }
